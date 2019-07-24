dfaa028c79550251ccbdf6a4b71a0a32a143778e
Guillermo Asaro <guillermoasaro@gmail.com>
2019-07-24 16:30:48 -0400

    add master key to capistrano linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :deploy_to, "/srv/apps/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 #append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
