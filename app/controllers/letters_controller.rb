85a5d8bdd0436603e140790248c854024278fb2b
pdrowr <pdrowr@gmail.com>
2019-08-30 13:51:12 -0400

    added letters module

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -3,16 +3,16 @@ class LettersController < ApplicationController
   before_action :set_countries, only: %i[new create edit update]
 
   def index
-    @letters = Airline.all.includes(:country)
+    @letters = Letter.all.includes(:country)
     respond_to_formats(:index, @letters)
   end
 
   def new
-    @letter = Airline.new
+    @letter = Letter.new
   end
 
   def create
-    @letter = Airline.new(letter_params)
+    @letter = Letter.new(letter_params)
 
     if @letter.save
       respond_to do |format|
@@ -32,7 +32,7 @@ class LettersController < ApplicationController
   def update
     respond_to do |format|
       if @letter.update(letter_params)
-        format.html { redirect_to @letter, notice: 'Airline was successfully updated.' }
+        format.html { redirect_to @letter, notice: 'Letter was successfully updated.' }
         format.json { render json: @letter, status: :ok }
       else
         format.html { render :edit }
@@ -48,7 +48,7 @@ class LettersController < ApplicationController
   def destroy
     @letter.destroy
     respond_to do |format|
-      format.html { redirect_to letters_url, notice: 'Airline was successfully destroyed.' }
+      format.html { redirect_to letters_url, notice: 'Letter was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -56,11 +56,11 @@ class LettersController < ApplicationController
   private
 
   def set_letter
-    @letter = Airline.find_by_id(params.dig(:id))
+    @letter = Letter.find_by_id(params.dig(:id))
   end
 
   def letter_params
-    params.require(:letter).permit(:name, :phone, :country_id)
+    params.require(:letter).permit(:text, :country_id)
   end
 
   def set_countries
