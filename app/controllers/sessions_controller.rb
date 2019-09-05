1a69d5ccce54f671ecf01902be25083698696848
pdrowr <pdrowr@gmail.com>
2019-09-05 15:23:03 -0400

    modified create method in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      if session[:controller] && session[:action]
-        flash[:alert] = "Logged in!"
-        begin
-          redirect_to controller: session[:controller], action: session[:action]
-        rescue ActionController::UrlGenerationError
-          redirect_to main_path
-        end
-      else
-        redirect_to main_path
+      redirect_to main_path
       end
     else
       flash.now[:alert] = "Email or password is invalid"
