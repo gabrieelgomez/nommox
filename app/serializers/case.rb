625812319906ab0a6960367a1b04ad2d81cce2ec
pdrowr <pdrowr@gmail.com>
2019-09-16 15:55:49 -0400

    remove duplicated file

--- app/serializers/case.rb
+++
@@ -1,7 +0,0 @@
-class Case < ApplicationRecord
-  belongs_to :user
-  has_one    :inconvenience, dependent: :destroy
-  has_one    :booking, dependent: :destroy
-  has_one    :tests, dependent: :destroy, class_name: 'Test'
-  has_many   :tickets, dependent: :destroy
-end
