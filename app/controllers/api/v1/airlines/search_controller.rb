3e0ab5a07d67d2b77a4a2cd0b30fdf429036b908
pdrowr <pdrowr@gmail.com>
2019-09-17 09:53:50 -0400

    search airlines

--- app/controllers/api/v1/airlines/search_controller.rb
+++ app/controllers/api/v1/airlines/search_controller.rb
@@ -1,11 +1,38 @@
 module Api::V1::Airlines
   class SearchController < AirlinesController
 
+    before_action :set_airlines, only: %i[filter_airlines]
+
     def find_airline
       query = params.dig(:query)
       @airline = Airline.where("name LIKE :query", query: "%#{query}%")
       render json: @airline, status: 200
     end
-    
+
+    def filter_airlines
+      query = params.dig(:query)&.upcase
+      @results = Array.new
+
+      @airlines.each do |a|
+        if a[0].include?(query) || a[1].upcase.include?(query)
+          @results.push(build_hash(a))
+        end
+      end
+
+      render json: @results
+    end
+
+    private
+
+    def build_hash(data)
+      temp_hash = Hash.new
+
+      temp_hash[:code] = data[0]
+      temp_hash[:name] = data[1]
+
+      temp_hash
+    end
+
+
   end
 end
