ac203b8557fe58427871ec99f2643aa4881d6c5c
pdrowr <pdrowr@gmail.com>
2019-09-24 13:53:42 -0400

    added redirect on click, image and sound to push notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -29,9 +29,32 @@ messaging.setBackgroundMessageHandler(function(payload) {
   // Customize notification here
   const notificationTitle = `Tienes un nuevo mensaje de ${payload.data.author}`;
   const notificationOptions = {
-    body: body
+    body:   body,
+    icon:   'https://nommox.com/assets/ItunesArtwork@2x.png',
+    sound:  'https://nommox.com/assets/notification.mp3'
   };
 
   return self.registration.showNotification(notificationTitle,
-      notificationOptions);
+    notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  let url = 'https://nommox.com/chat';
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then( windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        // If so, just focus it.
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 });
