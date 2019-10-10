fe6107351fae45bba66c4ad331f6225868056d2d
pdrowr <pdrowr@gmail.com>
2019-10-10 09:41:09 -0400

    removed unusued code

--- app/controllers/api/v1/devices/devices_controller.rb
+++ app/controllers/api/v1/devices/devices_controller.rb
@@ -1,11 +1,4 @@
 module Api::V1
   class Devices::DevicesController < ApiController
-
-    private
-
-    def set_device
-      @device = Device.find_by_identity(params.dig(:identity))
-    end
-
   end
 end
