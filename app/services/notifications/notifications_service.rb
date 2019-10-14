04abeb1f81ec5073dca38cb6cb6e45f08d8685e4
pdrowr <pdrowr@gmail.com>
2019-10-14 12:57:02 -0400

    added services to handle notifications and boarding pass parser

---
+++ app/services/notifications/notifications_service.rb
@@ -0,0 +1,27 @@
+module Notifications
+  class NotificationsService < ApplicationService
+
+    def initialize(token, message)
+     @token    = token
+     @message  = message
+    end
+
+    def self.send
+      return if @token.nil?
+
+      pusher = Grocer.pusher(
+        certificate: "#{Rails.root}/pushcert.pem",      # required,
+        gateway: 'gateway.push.apple.com',
+      )
+
+      notification = Grocer::Notification.new(
+        device_token: @token,
+        alert:        @message,
+        badge:        1,
+        sound:        "siren.aiff",         # optional
+      )
+
+      pusher.push(notification) # return value is the number of bytes sent successfully
+    end
+  end
+end
