78c92912651061eec2c47c94000af9f86006d6da
pdrowr <pdrowr@gmail.com>
2019-10-17 15:30:21 -0400

    transform keys

--- app/serializers/case_serializer.rb
+++ app/serializers/case_serializer.rb
@@ -1,11 +1,18 @@
 class CaseSerializer < ActiveModel::Serializer
   attribute :id
-  attribute :status_name, key: :status
-  attribute :video_self,  key: :video
-  attribute :case_causes, key: :causes
-  attributes :delayReason, :cancelTime, :overbookingBenefit, :baggageProblem,
-             :claimForm, :bills, :baggageBills, :baggageDate, :belongingsValue,
-             :valueStatement
+  attribute :status_name,        key: :status
+  attribute :video_self,         key: :video
+  attribute :case_causes,        key: :causes
+  attribute :delayReason,        key: :delayReason
+  attribute :cancelTime,         key: :cancelTime
+  attribute :overbookingBenefit, key: :overbookingBenefit
+  attribute :baggageProblem,     key: :baggageProblem
+  attribute :claimForm,          key: :claimForm
+  attribute :bills
+  attribute :baggageBills,       key: :baggageBills
+  attribute :baggageDate,        key: :baggageDate
+  attribute :belongingsValue,    key: :belongingsValue
+  attribute :valueStatement,     key: :valueStatement
 
   belongs_to :user
   has_one    :inconvenience
