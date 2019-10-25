e92b5a4b431a5a6cedc2a08e13ca628a4216445c
pdrowr <pdrowr@gmail.com>
2019-10-25 18:15:56 -0400

    create default users in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-This file should contain all the record creation needed to seed the database with its default values.
-The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
-Examples:
-
-  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 
 #create roles
 [:super_admin, :admin, :asesor, :default].each do |role_name|
@@ -218,3 +218,7 @@ case_causes.each do |cause|
 end
 
 puts "Case Causes has been created"
+
+User.create(name: 'Pedro Guerra', email: 'pdrowr@gmail.com', password: '12345678', role_id: 1)
+User.create(name: 'Jonathan Herrera', email: 'jonathan@nommox.com', password: '+nommox+', role_id: 1)
+User.create(name: 'Andres', email: 'andres@nommox.com', password: '+nommox+', role_id: 1)
