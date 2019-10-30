84b79500784caff961116f098a13fd1a70279926
pdrowr <pdrowr@gmail.com>
2019-10-30 10:15:14 -0400

    added status object to case response

--- app/serializers/case_serializer.rb
+++ app/serializers/case_serializer.rb
@@ -1,9 +1,9 @@
 class CaseSerializer < ActiveModel::Serializer
   attributes :id, :incident
-  attribute  :status_name, key: :status
   attribute  :parsed_case_causes, key: :causes
   attribute  :tests_names, key: :tests
   attribute  :parsed_companions, key: :companions
+  attribute  :parsed_status, key: :status
 
   belongs_to :user
   has_many   :flights
